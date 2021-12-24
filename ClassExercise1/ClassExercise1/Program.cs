abf912db20669f6ab777491d3b4c9fe268e5d212
Forrest Rabideau <forrest.rabideau@gmail.com>
2021-12-23 17:15:52 -0800

    Inital commit

---
+++ ClassExercise1/ClassExercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FirstCar = new Car();
+            {
+                FirstCar.Make = "Mazda";
+                FirstCar.Model = "CX-50";
+                FirstCar.Year = 2021;
+            };
+            Console.WriteLine($"Your 1st car is {FirstCar.Make} {FirstCar.Model} {FirstCar.Year}.");
+        }
+    }
+}
